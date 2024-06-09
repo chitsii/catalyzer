d7a2eb40d62607f62c697c3cc5a74ac597e8806a
FanJiang <59207213+chitsii@users.noreply.github.com>
2024-06-10 01:45:23 +0900

    add generics/types to apis

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -7,24 +7,28 @@ import { Settings } from "@/components/atoms";
 
 /// invoke_safe is a wrapper to safely execute invoke.
 /// We can only 'invoke' after window load when Next.js is used.
-export async function invoke_safe(command: string, arg_obj: any, default_response: any = null) {
+export async function invoke_safe<T>(command: string, arg_obj?: any, default_response?: T | undefined): Promise<T> {
   const isClient = typeof window !== "undefined";
-  if (!isClient) return default_response;
-  let res: any = await invoke(command, arg_obj)
-    .then(() => {
-      info(`invoke: ${command} done.`);
-    })
-    .catch((err) => {
-      error(err);
-      popUp("failed", err);
-      throw new Error(`found error: ${err}`);
-    });
-  return !!res ? res : default_response;
+  if (!isClient) return default_response as T;
+
+  try {
+    debug(`start invoke: ${command} with args: ${JSON.stringify(arg_obj)}`);
+    const res = await invoke<T>(command, arg_obj);
+    info(`invoke: ${command} done.`);
+    return res;
+  } catch (err) {
+    error(JSON.stringify(err));
+    popUp("failed", `failed to execute command: ${command}`);
+    throw new Error(`found error: ${err}`);
+  }
 }
 
 const launchGame = async () => await invoke_safe("launch_game", {});
+
 const openLocalDir = async (targetDir: string) => await invoke_safe("open_dir", { targetDir: targetDir });
+
 const openModData = async () => await invoke_safe("open_mod_data", {});
+
 const gitCommand = async (
   command: "git_init" | "git_commit_changes" | "git_reset_changes" | "git_list_branches" | "git_checkout",
   args: {
@@ -34,13 +38,20 @@ const gitCommand = async (
     createIfUnexist?: boolean;
   },
 ) => await invoke_safe(command, args);
+
 const listBranches = async (targetDir: string) => await invoke_safe("git_list_branches", { targetDir: targetDir }, []);
+
 const installMod = async (moddata_dir: string) => await invoke_safe("install_mod", { modDataPath: moddata_dir });
+
 const uninstallMod = async (moddata_dir: string) => await invoke_safe("uninstall_mod", { modDataPath: moddata_dir });
-const fetchMods = async () => await invoke_safe("scan_mods", []);
+
+const fetchMods = async () => await invoke_safe<Mod[]>("scan_mods", {}, []);
+
 const unzipModArchive = async (src: string, existsOk?: boolean) =>
   await invoke_safe("unzip_mod_archive", { src: src, existsOk: existsOk });
-const getSettings = async () => await invoke_safe("get_settings", {});
+
+const getSettings = async () => await invoke_safe<Settings>("get_settings");
+
 const addProfile = async (name: string, gamePath: string) => {
   const args = {
     id: createId(),
@@ -49,7 +60,9 @@ const addProfile = async (name: string, gamePath: string) => {
   };
   await invoke_safe("add_profile", args);
 };
+
 const removeProfile = async (profileId: string) => await invoke_safe("remove_profile", { profileId: profileId });
+
 const editProfile = async (profileId: string, name: string, gamePath: string) => {
   const args = {
     profileId: profileId,
@@ -58,8 +71,10 @@ const editProfile = async (profileId: string, name: string, gamePath: string) =>
   };
   await invoke_safe("edit_profile", args);
 };
+
 const setProfileActive = async (profileId: string) => await invoke_safe("set_profile_active", { profileId: profileId });
-const tailLog = async () => await invoke_safe("tail_log", []);
+
+const tailLog = async () => await invoke_safe<String[]>("tail_log", {}, []);
 
 export {
   gitCommand,
